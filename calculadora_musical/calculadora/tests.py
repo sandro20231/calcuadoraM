85f78e3397935d6662bdd449c6c60c446a8591ae
Sandro <sandrocoelho2023a@gmail.com>
2025-05-25 12:49:56 -0300

    traduzindo letras em números

--- calculadora_musical/calculadora/tests.py
+++ calculadora_musical/calculadora/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from .acoes import traduzir_numero
 # Create your tests here.
+
+
+class testes(TestCase):
+    # testes de tradução de letras em números
+    def testenota1(self):
+        """C é 1"""
+        self.assertEqual(traduzir_numero("C"), 1)
+
+    def testnota2(self):
+        """d é 3"""
+        self.assertEqual(traduzir_numero("d"), 3)
+
+    def testnota3(self):
+        """F# é 7"""
+        self.assertEqual(traduzir_numero("F#"), 7)
+
+    def testnota4(self):
+        """g# é 9"""
+        self.assertEqual(traduzir_numero("g#"), 9)
+
+    def testnota5(self):
+        """Gb é 7"""
+        self.assertEqual(traduzir_numero("Gb"), 7)
+
+    def testnota6(self):
+        """ab é 9"""
+        self.assertEqual(traduzir_numero("ab"), 9)
+    # testa de conexão com tela incial
+
+    def test_telainicial(self):
+        c = Client()
+        tela = c.get('')
+        self.assertEqual(tela.status_code, 200)
--- calculadora_musical/calculadora/views.py
+++ calculadora_musical/calculadora/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .acoes import traduzir_numero
 
 # Create your views here.
 
 
 def telainicial(request):
+    if request.method == "POST":
+        letra = request.POST['le1']
+        resposta = traduzir_numero(letra)
+        return render(request, 'calculadora/tela.html', {"resposta":resposta}) 
     return render(request, 'calculadora/tela.html')
