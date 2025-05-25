1ecf208be6045b35f14393c1556aa5be4c7810be
Sandro <sandrocoelho2023a@gmail.com>
2025-05-25 17:23:17 -0300

    adicionando tratamento de erros de usuários

--- calculadora_musical/calculadora/views.py
+++ calculadora_musical/calculadora/views.py
@@ -5,67 +5,86 @@ from .intervalos import tonica, segundamaior, tercamaior, quartajusta, quintajus
 
 
 def escalamaior(request):
-    if request.method == "POST":
-        letra = request.POST['le1']
-        grau1 = tonica(letra)
-        grau2 = segundamaior(letra)
-        grau3 = tercamaior(letra)
-        grau4 = quartajusta(letra)
-        grau5 = quintajusta(letra)
-        grau6 = sextamaior(letra)
-        grau7 = setimamaior(letra)
-        return render(request, 'calculadora/escalamaior.html', {"grau1": grau1,
-                                                                "grau2": grau2,
-                                                                "grau3": grau3,
-                                                                "grau4": grau4,
-                                                                "grau5": grau5,
-                                                                "grau6": grau6,
-                                                                "grau7": grau7})
-    return render(request, 'calculadora/escalamaior.html')
+    try:
+        if request.method == "POST":
+            ativado = True
+            letra = request.POST['le1']
+            grau1 = tonica(letra)
+            grau2 = segundamaior(letra)
+            grau3 = tercamaior(letra)
+            grau4 = quartajusta(letra)
+            grau5 = quintajusta(letra)
+            grau6 = sextamaior(letra)
+            grau7 = setimamaior(letra)
+            return render(request, 'calculadora/escalamaior.html', {"grau1": grau1,
+                                                                    "grau2": grau2,
+                                                                    "grau3": grau3,
+                                                                    "grau4": grau4,
+                                                                    "grau5": grau5,
+                                                                    "grau6": grau6,
+                                                                    "grau7": grau7,
+                                                                    "ativado": ativado})
+    except KeyError as e:
+        return render(request, "calculadora/erro.html", {"mensagem": "Precisa digitar uma letra que imboliza uma nota por exemplo para la pode ser a ou A"})
+    ativado = False
+    return render(request, 'calculadora/escalamaior.html', {"ativado": ativado})
 
 
 def menor(request):
-    if request.method == "POST":
-        letra = request.POST['le2']
-        grau1 = tonica(letra)
-        grau2 = segundamaior(letra)
-        grau3 = tercamenor(letra)
-        grau4 = quartajusta(letra)
-        grau5 = quintajusta(letra)
-        grau6 = sextamenor(letra)
-        grau7 = setimamenor(letra)
-        return render(request, "calculadora/menor.html", {
-            "grau1": grau1,
-            "grau2": grau2,
-            "grau3": grau3,
-            "grau4": grau4,
-            "grau5": grau5,
-            "grau6": grau6,
-            "grau7": grau7
-        })
-    return render(request, "calculadora/menor.html")
+    try:
+        ativado = True
+        if request.method == "POST":
+            letra = request.POST['le2']
+            grau1 = tonica(letra)
+            grau2 = segundamaior(letra)
+            grau3 = tercamenor(letra)
+            grau4 = quartajusta(letra)
+            grau5 = quintajusta(letra)
+            grau6 = sextamenor(letra)
+            grau7 = setimamenor(letra)
+            return render(request, "calculadora/menor.html", {
+                "grau1": grau1,
+                "grau2": grau2,
+                "grau3": grau3,
+                "grau4": grau4,
+                "grau5": grau5,
+                "grau6": grau6,
+                "grau7": grau7,
+                "ativado": ativado
+            })
+    except KeyError as e:
+        return render(request, "calculadora/erro.html", {"mensagem": "Precisa digitar uma letra que imboliza uma nota por exemplo para la pode ser a ou A"})
+    ativado = False
+    return render(request, "calculadora/menor.html", {"ativado": ativado})
 
 
 def menorharmonica(request):
-    if request.method == "POST":
-        letra = request.POST['le3']
-        grau1 = tonica(letra)
-        grau2 = segundamaior(letra)
-        grau3 = tercamenor(letra)
-        grau4 = quartajusta(letra)
-        grau5 = quintajusta(letra)
-        grau6 = sextamenor(letra)
-        grau7 = setimamaior(letra)
-        return render(request, "calculadora/menorharmonica.html", {
-            "grau1": grau1,
-            "grau2": grau2,
-            "grau3": grau3,
-            "grau4": grau4,
-            "grau5": grau5,
-            "grau6": grau6,
-            "grau7": grau7
-        })
-    return render(request, "calculadora/menorharmonica.html")
+    try:
+        ativado = True
+        if request.method == "POST":
+            letra = request.POST['le3']
+            grau1 = tonica(letra)
+            grau2 = segundamaior(letra)
+            grau3 = tercamenor(letra)
+            grau4 = quartajusta(letra)
+            grau5 = quintajusta(letra)
+            grau6 = sextamenor(letra)
+            grau7 = setimamaior(letra)
+            return render(request, "calculadora/menorharmonica.html", {
+                "grau1": grau1,
+                "grau2": grau2,
+                "grau3": grau3,
+                "grau4": grau4,
+                "grau5": grau5,
+                "grau6": grau6,
+                "grau7": grau7,
+                "ativado": ativado
+            })
+    except KeyError as e:
+        return render(request, "calculadora/erro.html", {"mensagem": "Precisa digitar uma letra que imboliza uma nota por exemplo para la pode ser a ou A"})
+
+    ativado = False
+    return render(request, "calculadora/menorharmonica.html", {"ativado": ativado})
 
 
 def index(request):
@@ -73,22 +92,29 @@ def index(request):
 
 
 def menormelodica(request):
-    if request.method == "POST":
-        letra = request.POST['le4']
-        grau1 = tonica(letra)
-        grau2 = segundamaior(letra)
-        grau3 = tercamenor(letra)
-        grau4 = quartajusta(letra)
-        grau5 = quintajusta(letra)
-        grau6 = sextamaior(letra)
-        grau7 = setimamaior(letra)
-        return render(request, "calculadora/menormelodica.html", {
-            "grau1": grau1,
-            "grau2": grau2,
-            "grau3": grau3,
-            "grau4": grau4,
-            "grau5": grau5,
-            "grau6": grau6,
-            "grau7": grau7
-        })
-    return render(request, "calculadora/menormelodica.html")
+    try:
+        ativado = True
+        if request.method == "POST":
+            letra = request.POST['le4']
+            grau1 = tonica(letra)
+            grau2 = segundamaior(letra)
+            grau3 = tercamenor(letra)
+            grau4 = quartajusta(letra)
+            grau5 = quintajusta(letra)
+            grau6 = sextamaior(letra)
+            grau7 = setimamaior(letra)
+            return render(request, "calculadora/menormelodica.html", {
+                "grau1": grau1,
+                "grau2": grau2,
+                "grau3": grau3,
+                "grau4": grau4,
+                "grau5": grau5,
+                "grau6": grau6,
+                "grau7": grau7,
+                "ativado": ativado
+            })
+    except KeyError as e:
+        return render(request, "calculadora/erro.html", {"mensagem": "Precisa digitar uma letra que imboliza uma nota por exemplo para la pode ser a ou A"})
+    ativado = False
+    ativado = False
+    return render(request, "calculadora/menormelodica.html",  {"ativado": ativado})
